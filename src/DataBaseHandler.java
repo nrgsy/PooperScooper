123f022912aa6e0ed3bf190a24627c1e7b4cf156
Thomas <tpech46@gmail.com>
2015-02-07 12:58:39 -0500

    yo

--- src/DataBaseHandler.java
+++ src/DataBaseHandler.java
@@ -1,10 +1,14 @@
 import java.net.UnknownHostException;
+import java.util.List;
+
+import org.bson.types.BasicBSONList;
 
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
 
@@ -14,11 +18,7 @@ public class DataBaseHandler {
 	public static void insertImage(
 			String dbName,
 			String collectionName,
-			String link,
-			String caption,
-			String timesAccessed,
-			String lastAccessDate
-			) throws UnknownHostException {
+			AssImage image) throws UnknownHostException {
 
 		System.out.println("inserting image");
 		MongoClient mongoClient = new MongoClient();
@@ -26,38 +26,59 @@ public class DataBaseHandler {
 		DBCollection dbCollection = db.getCollection(collectionName);
 
 		BasicDBList list = new BasicDBList();
-		list.add(new BasicDBObject("link", link));
-		list.add(new BasicDBObject("caption", caption));
+		list.add(new BasicDBObject("link", image.getLink()));
+		list.add(new BasicDBObject("caption", image.getCaption()));
 
 		BasicDBList list2 = new BasicDBList();
-		list2.add(new BasicDBObject("timeAccessed", timesAccessed));
-		list2.add(new BasicDBObject("lastAccess", lastAccessDate));
+		list2.add(new BasicDBObject("timesAccessed", image.getTimesAccessed()));
+		list2.add(new BasicDBObject("lastAccessDate", image.getLastAccessDate()));
 
 		BasicDBObject basicBitch = new BasicDBObject().append("contents", list).append("accessData", list2);
 
 		dbCollection.insert(basicBitch);
 	}
 	
-	//public static String get
+	public static AssImage getRandomAssImage(String dbName, String collectionName) throws UnknownHostException {
+		
+		System.out.println("scooping ass image");
+		MongoClient mongoClient = new MongoClient();
+		DB db = mongoClient.getDB(dbName);
+		DBCollection dbCollection = db.getCollection("collection1");
+
+		int randIndex = (int) (Math.random() * dbCollection.getCount());
+				
+		DBCursor dbCursor = dbCollection.find();
+
+		for (int i = 0; i < randIndex; i++) {
+			dbCursor.next();
+		}
+		
+		DBObject ass = dbCursor.next();
+				
+		BasicBSONList contents = (BasicBSONList) ass.get("contents");
+		BasicBSONList accessData = (BasicBSONList) ass.get("accessData");
+		
+		String link = (String) ((BasicDBObject) contents.get("0")).get("link");
+		String caption = (String) ((BasicDBObject) contents.get("1")).get("caption");
+		int timesAccessed = (int) ((BasicDBObject) accessData.get("0")).get("timesAccessed");
+		String lastAccessDate = (String) ((BasicDBObject) accessData.get("1")).get("lastAccessDate");
+
+		return new AssImage(link, caption, timesAccessed, lastAccessDate);		
+	}
 
 	public static void main(String[] args) throws UnknownHostException {
 
-		insertImage("test", "collection1", "www.yyyy.com", "yo mama so fat", "0", "02/07/15");
-
-		System.out.println("querying");
-		MongoClient mongoClient = new MongoClient();
-		DB db = mongoClient.getDB( "test" );
-		DBCollection dbCollection = db.getCollection("collection1");
-		DBCursor dbCursor = dbCollection.find();
-
-		while(dbCursor.hasNext()) {
-
-			System.out.println(dbCursor.next());
-
-
-		}
-
-
+		
+		//insertImage("test", "collection1", new AssImage("www.smokeWeed.com", "everyday", 0, "02/07/15"));
+		
+		AssImage i = getRandomAssImage("test", "collection1");
+		System.out.println(i.getCaption());
+		System.out.println(i.getLink());
+		System.out.println(i.getLastAccessDate());
+		System.out.println(i.getTimesAccessed());
+		
+		
+		
 
 		//		MongoClient mongoClient = new MongoClient();
 		//		mongoClient.dropDatabase("mydb");
@@ -67,9 +88,5 @@ public class DataBaseHandler {
 		//		DBCollection coll = db.getCollection("Channel");
 		//		coll.drop();
 		//		System.out.println(db.getCollectionNames());
-
-
-
 	}
-
 }
