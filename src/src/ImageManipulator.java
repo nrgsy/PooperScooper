ed4f8ad6e1f2b95b2e7cfefd1f5c7b7b1c99b3b7
pkhxcp <pkhxcp@gmail.com>
2015-02-07 13:33:34 -0500

    Repurposed imagemanipulator and enhanced redditscraper

--- src/src/ImageManipulator.java
+++ src/src/ImageManipulator.java
@@ -7,69 +7,83 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 
 
 public class ImageManipulator {
-	
-	
-	
-	
-	
-	private BufferedImage convertCMYK2RGB(BufferedImage image) throws IOException{
-	    //Create a new RGB image
-	    BufferedImage rgbImage = new BufferedImage(image.getWidth(), image.getHeight(),
-	    BufferedImage.TYPE_3BYTE_BGR);
-	    // then do a funky color convert
-	    ColorConvertOp op = new ColorConvertOp(null);
-	    op.filter(image, rgbImage);
-	    return rgbImage;
-	}
-	
-	
-	
-	
 
-	public ArrayList<ArrayList<String>> refactoredContent(){
-		
-		//TODO get last # in content pool 
+	File img = null;
+	int inc = 0;
+
+
+	public boolean isValid(String URI){
 		Image image = null;
-		int inc = 0;
 		
-		ArrayList<ArrayList<String>> poopscoop = new RedditScraper().contentSnatch();
-		for(String imgsrc : poopscoop.get(1)){
-		try {
-		    URL url = new URL(imgsrc);
-		    //URL url = new URL("i.imgur.com/Z9f6L5n.jpg");
-
-		    image = ImageIO.read(url);
-		    BufferedImage bi = (BufferedImage) image;
-		    
-		    if(!imgsrc.endsWith(".jpg")||!imgsrc.endsWith(".jpeg")){
-		    	bi = convertCMYK2RGB(bi);
-		    }
-		    
-		    File f = new File("pics/"+inc+".jpg");
-		    ImageIO.write(bi, "jpg", f);
-		    imgsrc = "pics/"+inc+".jpg";
-		    inc++;
-		    
+		try{
+			URL url = new URL(URI);
+			image = ImageIO.read(url);
+			BufferedImage bi = (BufferedImage) image;
+			img = new File("pics/"+inc+".jpg");
+			inc++;
+			ImageIO.write(bi, "jpg", img);
+			//Checks to see img size is less than ~3MB
+			if(img.length()<3000000){
+				System.out.println("image is less than 3MB #"+inc);
+				return true;
+			}
+			
 		}
-		catch (IOException e) {
+		catch(IOException e) {
 			System.out.println("fuckin up kp");
 		}
+		finally{
+			img.delete();
 		}
-		return poopscoop;
-		
-		
+		System.out.println("Image size of "+URI+"is larger than 3MB");
+		return false;
 	}
-	
-	
-	public static void main(String[] args){
-		for(String imgsrc : new ImageManipulator().refactoredContent().get(1)){
-			System.out.println(imgsrc);
-		}
+
+
+	private BufferedImage convertCMYK2RGB(BufferedImage image) throws IOException{
+		//Create a new RGB image
+		BufferedImage rgbImage = new BufferedImage(image.getWidth(), image.getHeight(),
+				BufferedImage.TYPE_3BYTE_BGR);
+		// then do a funky color convert
+		ColorConvertOp op = new ColorConvertOp(null);
+		op.filter(image, rgbImage);
+		return rgbImage;
 	}
-	
+
+
+	public String getImageFile(String imgsrc){
+
+		Image image = null;
+
+			try {
+				URL url = new URL(imgsrc);
+				image = ImageIO.read(url);
+				BufferedImage bi = (BufferedImage) image;
+
+				//If not jpg, then colorconvert to avoid red tint
+				if(!imgsrc.endsWith(".jpg")||!imgsrc.endsWith(".jpeg")){
+					bi = convertCMYK2RGB(bi);
+				}
+				Random rand = new Random();
+			    int randomNum = rand.nextInt(5001);
+				File f = new File("pics/"+randomNum+".jpg");
+				ImageIO.write(bi, "jpg", f);
+				imgsrc = "pics/"+randomNum+".jpg";
+				return imgsrc;
+				//The receiver must delete the file after posting to Twitter
+			}
+			catch (IOException e) {
+				System.out.println("fuckin up kp");
+			}
+		System.out.println("Error: Did not get image file with string"+imgsrc);
+		return "error";
+
+	}
+
 }
