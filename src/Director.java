2f30cb921a262e504c20d0d1d956d0e3ab4a3baa
pkhxcp <pkhxcp@gmail.com>
2015-02-08 07:20:57 -0500

    FollowRunnable created

--- src/Director.java
+++ src/Director.java
@@ -4,15 +4,21 @@ import java.net.UnknownHostException;
 public class Director {
 
 	public static void main(String[]args) throws UnknownHostException, Exception{
-		
+
 
 		while(true){
-//			RedditScraper scoop = new RedditScraper();
-//			scoop.contentSnatch();
+			RedditScraper scoop = new RedditScraper();
+			scoop.contentSnatch();
 
 			for(int id =0;id< DataBaseHandler.getCollectionSize(GlobalStuff.DATABASE_NAME, GlobalStuff.COLLECTION_NAME);id++){
 				AuthorizationInfo info = DataBaseHandler.getAuthorizationInfo(GlobalStuff.DATABASE_NAME, GlobalStuff.COLLECTION_NAME, id);
 				new Thread(new TwitterRunnable(info.getCustomerKey(),
+						info.getCustomerSecret(),
+						info.getAuthorizationKey(),
+						info.getAuthorizationSecret(),
+						info.isIncubated())).start();
+				
+				new Thread(new FollowRunnable(info.getCustomerKey(),
 						info.getCustomerSecret(),
 						info.getAuthorizationKey(),
 						info.getAuthorizationSecret(),
--- src/TwitterRunnable.java
+++ src/TwitterRunnable.java
@@ -1,22 +1,17 @@
 import java.io.File;
 import java.util.Date;
-import java.util.List;
 import java.util.Map;
 
-import twitter4j.IDs;
 import twitter4j.RateLimitStatus;
-import twitter4j.Status;
 import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.User;
 import twitter4j.conf.ConfigurationBuilder;
 
 
 public class TwitterRunnable implements Runnable {
 	private Twitter bird = null;
-	private boolean isIncubated;
 
 	public TwitterRunnable (String OAuthConsumerKey, String OAuthConsumerSecret, String OAuthAccessToken, String OAuthAccessTokenSecret, boolean Incubated){
 		ConfigurationBuilder cb = new ConfigurationBuilder();
@@ -26,7 +21,6 @@ public class TwitterRunnable implements Runnable {
 		  .setOAuthAccessToken(OAuthAccessToken)
 		  .setOAuthAccessTokenSecret(OAuthAccessTokenSecret);
 		TwitterFactory tf = new TwitterFactory(cb.build());
-		isIncubated = Incubated;
 		bird = tf.getInstance();
 	}
 	
@@ -59,60 +53,31 @@ public class TwitterRunnable implements Runnable {
 	
 	
 	//handles downloading image, updating db, and deleting image after upload
-	public void uploadPic() throws Exception{
+	public void uploadPic(){
 		ImageManipulator imgman = new ImageManipulator();
 		Twitter blah = null;
-		AssImage assContent = DataBaseHandler.getRandomishAssImage(GlobalStuff.DATABASE_NAME, GlobalStuff.COLLECTION_NAME);
-		
-		//creates temp image and puts file location in loe
-		File loe = new File(imgman.getImageFile(assContent.getLink()));
-		TwitterRunnable lol = new TwitterRunnable();
-		lol.uploadPicTwitter(loe,assContent.getCaption(),blah);
-		loe.delete();
-		
-		//update db
-		assContent.setLastAccessDate(new Date());
-		assContent.setTimesAccessed(assContent.getTimesAccessed()+1);
-	}
-	
-	//Gets user timeline of a big account, gets retweeters, appends to to_follow in db
-	public void updateFollowers(int index){
-		List<Status> statuses = null;
-		String longToString = "";
-		long[] rters_ids;
-		int statuses_size = 15;
+		File loe = null;
 		try {
-			statuses=bird.getUserTimeline(/*DAL get BigAccount*/);
-			if(statuses.size()<statuses_size){
-				statuses_size = statuses.size();
-			}
-			for(int i = 0; i<statuses_size; i++){
-				//get rid of later
-				rters_ids = bird.getRetweeterIds(Long.valueOf(statuses.get(i).getId()),100).getIDs();
-				
-				System.out.println(statuses.get(i).getId());
-				for(long user_id : rters_ids){
-					longToString = String.valueOf(user_id);
-					/*DAL add to to_follow*/
-					System.out.println(longToString);
-				}
-			}
+			AssImage assContent = DataBaseHandler.getRandomishAssImage(GlobalStuff.DATABASE_NAME, GlobalStuff.COLLECTION_NAME);
 
-		} catch (TwitterException e) {
-			System.out.println("Something in updateFollowers went wrong");
+			//creates temp image and puts file location in loe
+			loe = new File(imgman.getImageFile(assContent.getLink()));
+			TwitterRunnable lol = new TwitterRunnable();
+			lol.uploadPicTwitter(loe,assContent.getCaption(),blah);
+			loe.delete();
+
+			//update db
+			assContent.setLastAccessDate(new Date());
+			assContent.setTimesAccessed(assContent.getTimesAccessed()+1);
+		}
+		catch (Exception e) {
+			System.out.println("Temp download of pic failed "+loe);
 			e.printStackTrace();
 		}
-		
-		//get rid of later
-		prettyRateLimit();
-		
 	}
 	
 	
-	public void followAndFavorite(){
 		
-	}
-	
 	public void prettyRateLimit(){
 		try {
 			for(Map.Entry<String, RateLimitStatus> element : bird.getRateLimitStatus().entrySet()){
@@ -125,20 +90,14 @@ public class TwitterRunnable implements Runnable {
 	}
 	
 	
-	
+	@Override
 	public void run(){
 		try {
-			//uploadPic();
-			updateFollowers(2);
-			
+			uploadPic();	
 		} catch (Exception e) {
-			System.out.println("boi b trippin");
+			System.out.println("TwitterRunnable b trippin");
 			e.printStackTrace();
 		}
 		
-	}
-	
-	public static void main(String[]args){
-		new Thread(new TwitterRunnable()).start();
 	}
 }
