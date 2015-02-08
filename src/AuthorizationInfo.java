ccb081e337fba416bb6069ed23144d257348cecc
pkhxcp <pkhxcp@gmail.com>
2015-02-08 09:01:20 -0500

    merge already

--- src/AuthorizationInfo.java
+++ src/AuthorizationInfo.java
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-
-
-
-
-//TODO make sure setters actually update attribute in database
-
-
-
-
-public class AuthorizationInfo {
-	
-	private String customerSecret;
-	
-	private String customerKey;
-	
-	private String authorizationSecret;
-	
-	private String authorizationKey;
-	
-	private boolean isIncubated;
-	
-	public AuthorizationInfo(
-			String customerSecret,
-			String customerKey,
-			String authorizationSecret,
-			String authorizationKey,
-			boolean isIncubated) {
-		super();
-		this.customerSecret = customerSecret;
-		this.customerKey = customerKey;
-		this.authorizationSecret = authorizationSecret;
-		this.authorizationKey = authorizationKey;
-		this.isIncubated = isIncubated;
-	}
-
-	public String getCustomerSecret() {
-		return customerSecret;
-	}
-
-	public String getCustomerKey() {
-		return customerKey;
-	}
-
-	public String getAuthorizationSecret() {
-		return authorizationSecret;
-	}
-
-	public String getAuthorizationKey() {
-		return authorizationKey;
-	}
-
-	public synchronized boolean isIncubated() {
-		return isIncubated;
-	}
-
-	public synchronized void setIncubated(boolean isIncubated) {
-		this.isIncubated = isIncubated;
-	
-		
-//TODO update in the actual database
-		
-		
-		
-		
-	}
-}
-=======
 
 
 
@@ -129,4 +61,3 @@ public class AuthorizationInfo {
 		
 	}
 }
->>>>>>> 5b0292d18bc0fea6f08c7b082145bf8ab00d5ef9
